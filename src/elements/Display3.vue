383e042f78f933a05af6b338c97725c3f704117f
an.luu <an.luu@messagemedia.com>
2021-12-08 13:29:41 +0700

    feat: add display and title component

---
+++ src/elements/Display3.vue
@@ -0,0 +1,43 @@
+<template>
+  <component :is="type" class="display3">
+    <slot></slot>
+  </component>
+</template>
+
+<script>
+export default {
+  name: "Display3",
+  status: "prototype",
+  release: "3.5.0",
+  props: {
+    type: {
+      type: String,
+      default: "h3",
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.display3 {
+  font-family: var(--heading-font);
+  font-style: normal;
+  font-weight: normal;
+  font-size: 32px;
+  line-height: 39px;
+  text-transform: uppercase;
+  color: var(--primary-text);
+  margin-bottom: 24px;
+
+  @include tablet {
+    font-size: 24px;
+    line-height: 30px;
+  }
+
+  @include mobile {
+    font-size: 24px;
+    line-height: 32px;
+    margin-bottom: 30px;
+  }
+}
+</style>
---
+++ src/elements/Title2.vue
@@ -0,0 +1,43 @@
+<template>
+  <component :is="type" class="title2">
+    <slot></slot>
+  </component>
+</template>
+
+<script>
+export default {
+  name: "Title2",
+  status: "prototype",
+  release: "3.5.0",
+  props: {
+    type: {
+      type: String,
+      default: "h3",
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.title2 {
+  font-family: var(--heading-font);
+  font-weight: normal;
+  font-size: 18px;
+  line-height: 30px;
+  line-height: 43px;
+  text-transform: uppercase;
+  color: var(--primary-text);
+  margin-bottom: 0;
+
+  @include tablet {
+    font-size: 24px;
+    line-height: 30px;
+  }
+
+  @include mobile {
+    font-size: 24px;
+    line-height: 32px;
+    margin-bottom: 30px;
+  }
+}
+</style>
