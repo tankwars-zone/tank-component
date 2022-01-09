6aa374732eaa7134214f5f16945e2d84f256ac61
an.luu <an.luu@messagemedia.com>
2022-01-09 20:00:00 +0700

    feat: title component

---
+++ src/elements/Title1.vue
@@ -0,0 +1,58 @@
+<template>
+  <component :is="type" class="title1" :class="[className, `title1--${variant}`]">
+    <slot></slot>
+  </component>
+</template>
+
+<script>
+export default {
+  name: "Title1",
+  status: "prototype",
+  release: "3.5.0",
+  props: {
+    type: {
+      type: String,
+      default: "h4",
+    },
+    className: {
+      type: String,
+      default: "",
+    },
+    variant: {
+      type: String,
+      default: "normal",
+      validator: val => ["light", "normal", "primary"].includes(val),
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.title1 {
+  font-family: var(--heading-font);
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 32px;
+  color: var(--primary-text);
+  margin-bottom: 8px;
+
+  &--light {
+    color: #fff;
+  }
+
+  &--primary {
+    color: var(--color-primary);
+  }
+
+  @include tablet {
+    font-size: 24px;
+    line-height: 30px;
+  }
+
+  @include mobile {
+    font-size: 24px;
+    line-height: 32px;
+    margin-bottom: 30px;
+  }
+}
+</style>
